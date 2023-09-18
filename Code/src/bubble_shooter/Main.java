f80292ba1028b9aa92df7db8e6be694554a37068
anjanshrestha8 <anjanshrestha2023@gmail.com>
2023-09-18 18:54:46 +0545

    presentation and documentation

---
+++ Code/src/bubble_shooter/Main.java
@@ -0,0 +1,18 @@
+package bubble_shooter;
+
+import java.awt.GridLayout;
+
+import javax.swing.SwingUtilities;
+
+/**
+ * mainclass, simply initiates everything
+ * @author Anjan
+ *
+ */
+public class Main {
+    public static void main(String[] args) {
+        MainFrame gameFrame = new MainFrame();
+        gameFrame.init();
+//        SplashScreen obj = new SplashScreen();
+    }
+}
